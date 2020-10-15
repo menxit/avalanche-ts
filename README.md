e70bb9e8475161a00221ec1a8db771f448da7d39
Federico Ginosa <federico@menxit.com>
2020-10-15 16:41:55 +0200

    commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # avalanche-ts
+
+```
+import { rpc } from "avalanche-ts'
+
+rpc.avm.getTx({ txID: "2k2ZFBdBdMGZqonGh8WHsHbKYNPHM2cAjY9etRK2vcdP5Eda4m" }).then((res: any) => {
+  console.log(res);
+});
+```
--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,5 @@
-import { avm } from '../src'
+import { rpc } from '../src'
 
-avm.getTx({ txID: "2k2ZFBdBdMGZqonGh8WHsHbKYNPHM2cAjY9etRK2vcdP5Eda4m" }).then((res: any) => {
+rpc.avm.getTx({ txID: "2k2ZFBdBdMGZqonGh8WHsHbKYNPHM2cAjY9etRK2vcdP5Eda4m" }).then((res: any) => {
   console.log(res);
 });
